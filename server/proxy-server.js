2e3df45a1366f76a18af86dfbbd9183f75d1ec90
ozzychel <andrey.petunin86@gmail.com>
2020-06-30 19:02:30 -0700

    apply whole page styling

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -11,8 +11,6 @@ const serverTwo = 'http://localhost:3002';
 const serverThree = 'http://localhost:3003';
 const serverFour = 'http://localhost:3004';
 
-
-
 app.all('/api/calendar/db/*', (req, res) => {
   console.log('redirecting to Calendar server');
   apiProxy.web(req, res, {target: serverOne, changeOrigin: true});
@@ -23,7 +21,7 @@ app.all('/api/:hotelID/photos', (req, res) => {
   apiProxy.web(req, res, {target: serverTwo, changeOrigin: true});
 });
 
-app.all('/api/photos/', (req, res) => {
+app.all('/api/photos/*', (req, res) => {
   console.log('redirecting to About server');
   apiProxy.web(req, res, {target: serverThree, changeOrigin: true});
 });
@@ -34,8 +32,8 @@ app.all('/reviews/*', (req, res) => {
   apiProxy.web(req, res, {target: serverFour, changeOrigin: true});
 });
 
-
 app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Proxy listening on http://localhost:${port}`));
 
 app.use(morgan('dev'));
