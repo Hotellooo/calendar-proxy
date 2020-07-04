8d8926eaf9a244decc72fc47b94b4154aa39c959
ozzychel <andrey.petunin86@gmail.com>
2020-07-03 17:11:53 -0700

    apply styling

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -6,14 +6,10 @@ const parser = require('body-parser');
 
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
-// const serverOne = 'http://localhost:3001';
-// const serverTwo = 'http://localhost:3002';
-// const serverThree = 'http://localhost:3003';
-// const serverFour = 'http://localhost:3004';
 const serverOne = 'http://ec2-3-17-163-130.us-east-2.compute.amazonaws.com/';
 const serverTwo = 'http://ec2-18-217-154-181.us-east-2.compute.amazonaws.com/';
 const serverThree = 'http://ec2-54-241-67-8.us-west-1.compute.amazonaws.com/';
-const serverFour = 'http://ec2-13-57-249-34.us-west-1.compute.amazonaws.com/reviews/';
+const serverFour = 'http://13.57.249.34/reviews/';
 
 app.all('/api/calendar/db/*', (req, res) => {
   console.log('redirecting to Calendar server');
